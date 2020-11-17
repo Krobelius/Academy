a61430a1e06a2624f3d9a367a7e9733dac25e51b
Krobelius <oshargorodskiy@list.ru>
2020-11-17 17:54:28 +0300

    Добавил предупреждение о пустых полях в авторизации, но надо ещё доработать

---
+++ ProjectAcademy/ProjectAcademy/Content/Scrpits/ScriptsForAuth.js
@@ -0,0 +1,10 @@
+﻿let btn_auth = document.getElementById('auth_btn_one');
+btn_auth.onclick = function () {
+    let phone = document.getElementById('phone');
+    let pass = document.getElementById('inputPassword');
+    if (phone.nodeValue || pass.nodeValue == null) {
+        const header_for_errmsg = document.getElementById('msg_error');
+        header_for_errmsg.setAttribute('style', 'display:inline');
+        
+    }
+};
